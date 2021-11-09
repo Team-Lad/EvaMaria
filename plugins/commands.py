409a4f5ca60f9ff865238286252f60ce33a6d7c7
POISON <86126808+Cat-of-Tg@users.noreply.github.com>
2021-11-09 15:07:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -32,19 +32,18 @@ async def start(client, message):
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))       
             await db.add_chat(message.chat.id, message.chat.title)
         return 
-    if not await db.is_user_exist(message.from_user.id):
+     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
-   if len(message.command) != 2:
+    if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('🎭 MOVIE GROUP 🎭', url='https://t.me/movie_X_zone' )
+            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('SEARCH 👀', switch_inline_query_current_chat='')
+            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/EvaMariaUpdates')
             ],[
-            InlineKeyboardButton('ABOUT ME 🧸', callback_data='about'),
-            InlineKeyboardButton('HELP 🛠', callback_data='help')
-            ],[
-            InlineKeyboardButton('🚀 ADD ME TO YOUR GROUP 🚀', url=f'http://t.me/mx_filter_bot?startgroup=true')
+            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
+            InlineKeyboardButton('😊 About', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
