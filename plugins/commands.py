ce22f2beec1452896d19c48f8e5e31640610b24b
POISON <86126808+Cat-of-Tg@users.noreply.github.com>
2021-11-09 14:44:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -36,7 +36,7 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons = [[
+        buttons = [[[
             InlineKeyboardButton('🚀 ADD ME TO YOUR GROUP 🚀', url='http://t.me/mx_filter_bot?startgroup=true')
             ],[
             InlineKeyboardButton('SEARCH 👀', switch_inline_query_current_chat='')
@@ -45,8 +45,7 @@ async def start(client, message):
             InlineKeyboardButton('ABOUT ME 🧸', callback_data='about')
             [
             InlineKeyboardButton('🎭 MOVIE GROUP 🎭', url='https://t.me/movie_x_zone')
-        ]] 
-            
+            ]]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
