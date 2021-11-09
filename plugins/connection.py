e65d8a5ea211b6bf3599eb0b0ea0999d6ae6f9fa
POISON <86126808+Cat-of-Tg@users.noreply.github.com>
2021-11-09 12:48:40 +0530

    mmm

--- plugins/connection.py
+++ plugins/connection.py
@@ -2,10 +2,14 @@ from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.connections_mdb import add_connection, all_connections, if_active, delete_connection
 from info import ADMINS
-
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.ERROR)
 @Client.on_message((filters.private | filters.group) & filters.command('connect'))
 async def addconnection(client,message):
-    userid = message.from_user.id
+    userid = message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
     chat_type = message.chat.type
 
     if chat_type == "private":
@@ -33,7 +37,7 @@ async def addconnection(client,message):
             await message.reply_text("You should be an admin in Given group!", quote=True)
             return
     except Exception as e:
-        print(e)
+        logger.exception(e)
         await message.reply_text(
             "Invalid Group ID!\n\nIf correct, Make sure I'm present in your group!!",
             quote=True,
@@ -67,14 +71,16 @@ async def addconnection(client,message):
         else:
             await message.reply_text("Add me as an admin in group", quote=True)
     except Exception as e:
-        print(e)
+        logger.exception(e)
         await message.reply_text('Some error occured! Try again later.', quote=True)
         return
 
 
 @Client.on_message((filters.private | filters.group) & filters.command('disconnect'))
 async def deleteconnection(client,message):
-    userid = message.from_user.id
+    userid = message.from_user.id if message.from_user else None
+    if not userid:
+        return await message.reply(f"You are anonymous admin. Use /connect {message.chat.id} in PM")
     chat_type = message.chat.type
 
     if chat_type == "private":
